0dc03ca410583e3c7b64ce5121cc74009ae4eb2d
Eduardo Amorim <eduardo_amorim@outlook.com>
2023-08-08 11:28:37 -0300

    Começo do desenvolvimento desse algoritmo

---
+++ conta_vogais.py
@@ -0,0 +1,12 @@
+def conta_vogais(teste):
+    
+    contagem = 1
+
+    return contagem
+
+
+
+
+palavra = input("Digite a palavra: ")
+conta_vogais(palavra)
+print(f"A palavra {palavra} tem {contagem} vogais.")
